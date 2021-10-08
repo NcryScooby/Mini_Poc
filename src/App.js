363ce9c5329637a26f38731b16f92104f3fdb097
Renato Carvalho Costa <rcosta@desenvvar16-lnx.dimed.local>
2021-10-08 13:48:23 -0300

    #1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import DigitarNome from './components/DigitarNome';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <DigitarNome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MostrarNome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+export default class MostrarNome extends Component {
+
+
+  render() {
+    return (
+      <>
+        <p>ddasdasd</p>
+      </>
+    );
+  }
+}
